366c4c8bdbbfbe77b7411193280435485932eddf
fuhaous <fuhaous@163.com>
2018-04-09 23:30:32 +0800

    001

--- src/gui_test/JFrm.java
+++ src/gui_test/JFrm.java
@@ -9,11 +9,13 @@ public class JFrm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame jfr1 = new JFrame();
-		jfr1.setTitle("Im title");
+		jfr1.setTitle("Einstein");
 		jfr1.setSize(360,240);
 		jfr1.setLocation(500, 500);
 		jfr1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		jfr1.setVisible(true);
+		//new
 	}
 	
 }
