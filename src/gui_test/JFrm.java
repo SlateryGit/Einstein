1cb4fa55458ffebaaf29ec6a53a85cb666cba050
fuhaous <fuhaous@163.com>
2018-04-29 09:51:48 +0800

    panel

--- src/gui_test/JFrm.java
+++ src/gui_test/JFrm.java
@@ -1,21 +1,53 @@
 package gui_test;
-import java.awt.*;
-
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.*;
+//import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class JFrm {
+public class JFrm extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JButton buttons[];
+	private Container container;
+	private GridLayout jfr1;
+	//grid1 = new Grid
+	public JFrm() {
+		
+		//JFrame jfr1 = new JFrame();
+		
+		setTitle("Einstein");
+		setSize(360,240);
+		setLocation(500, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfr1 = new GridLayout(5, 5, 1, 1);
+		container=getContentPane();
+		container.setLayout(jfr1);
+		buttons = new JButton[25];
+		int i;
+		for(i=0;i<25;i++) {
+			buttons[i]=new JButton("0");
+			//buttons[i].addActionListener(this);
+			container.add(buttons[i]);
+		}
+		
+		setVisible(true);
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame jfr1 = new JFrame();
-		jfr1.setTitle("Einstein");
-		jfr1.setSize(360,240);
-		jfr1.setLocation(500, 500);
-		jfr1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		jfr1.setVisible(true);
+		JFrm App1 = new JFrm();
+		App1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//new
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
---
+++ src/gui_test/Table.java
@@ -0,0 +1,11 @@
+package gui_test;
+
+public class Table {
+
+	public Table() {
+		// TODO Auto-generated constructor stub
+		Chess = new int[5][5];
+	}
+	public int Chess[][];//正数代表A方，先手；负数代表B方，
+	
+}
